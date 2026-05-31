af8c45bf7fef72e81e29294b40271130ce666981
Olena Sobolieva <soboleva13as@gmail.com>
2026-05-31 23:39:28 +0300

    feat: implement user creation validation

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,6 +2,36 @@ import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
+   const bodyFields = Object.keys(req.body);
+
+   const requiredFields = [
+     "firstName",
+     "lastName",
+     "email",
+     "phone",
+     "password",
+   ];
+  
+  if (bodyFields.includes("id")) {
+    throw Error("User entity to create isn't valid");
+  }
+  
+  if (!requiredFields.every(field => bodyFields.includes(field))) {
+    throw Error("User entity to create isn't valid");
+  }
+  if (bodyFields.some((field) => !Object.keys(USER).includes(field))) {
+    throw Error("User entity to create isn't valid");
+  }
+  if (req.body.password.length < 3) {
+    throw Error("User entity to create isn't valid");
+  }
+  if (!req.body.email.endsWith("@gmail.com")) {
+    throw Error("User entity to create isn't valid");
+  }
+  if (!/^\+380\d{9}$/.test(req.body.phone)) {
+    throw Error("User entity to create isn't valid");
+  }
+
   next();
 };
 
