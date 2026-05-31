74986f80ba91c4982b14c923d1481bb89ba0e572
Olena Sobolieva <soboleva13as@gmail.com>
2026-05-31 21:04:22 +0300

    feat: implement user CRUD routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,5 +9,78 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get( "/",
+  (req, res, next) => {
+    try {
+      const data = userService.getAll();
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    
+    try {
+      const data = userService.getOne(req.params.id);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.post(
+  "/", createUserValid,
+  (req, res, next) => {
+    try {
+      
+      res.data = userService.create(req.body);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.patch(
+  "/:id",
+  updateUserValid,
+  (req, res, next) => {
+    try {
+      res.data = userService.update(req.params.id, req.body);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      res.data = userService.delete(req.params.id);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
 
 export { router };
