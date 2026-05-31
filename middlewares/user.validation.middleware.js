8131816dcd1b057fb2f697892a3cd5f88e690181
Olena Sobolieva <soboleva13as@gmail.com>
2026-06-01 02:34:45 +0300

    fix: handle validation errors in user middleware

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,34 +2,40 @@ import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
-   const bodyFields = Object.keys(req.body);
+  const bodyFields = Object.keys(req.body);
 
-   const requiredFields = [
-     "firstName",
-     "lastName",
-     "email",
-     "phone",
-     "password",
-   ];
-  
+  const requiredFields = [
+    "firstName",
+    "lastName",
+    "email",
+    "phone",
+    "password",
+  ];
+
   if (bodyFields.includes("id")) {
-    throw Error("User entity to create isn't valid");
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
-  
-  if (!requiredFields.every(field => bodyFields.includes(field))) {
-    throw Error("User entity to create isn't valid");
+  if (!requiredFields.every((field) => bodyFields.includes(field))) {
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
+
   if (bodyFields.some((field) => !Object.keys(USER).includes(field))) {
-    throw Error("User entity to create isn't valid");
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
   if (req.body.password.length < 3) {
-    throw Error("User entity to create isn't valid");
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
   if (!req.body.email.endsWith("@gmail.com")) {
-    throw Error("User entity to create isn't valid");
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
   if (!/^\+380\d{9}$/.test(req.body.phone)) {
-    throw Error("User entity to create isn't valid");
+    res.err = Error("User entity to create isn't valid");
+    return next();
   }
 
   next();
@@ -39,22 +45,28 @@ const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
   const bodyFields = Object.keys(req.body);
   if (bodyFields.includes("id")) {
-    throw Error("User entity to update isn't valid");
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
-   if (bodyFields.length === 0) {
-     throw Error("User entity to update isn't valid");
+  if (bodyFields.length === 0) {
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
   if (bodyFields.some((field) => !Object.keys(USER).includes(field))) {
-    throw Error("User entity to update isn't valid");
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
   if (req.body.password && req.body.password.length < 3) {
-    throw Error("User entity to update isn't valid");
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
   if (req.body.email && !req.body.email.endsWith("@gmail.com")) {
-    throw Error("User entity to update isn't valid");
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
   if (req.body.phone && !/^\+380\d{9}$/.test(req.body.phone)) {
-    throw Error("User entity to update isn't valid");
+    res.err = Error("User entity to update isn't valid");
+    return next();
   }
 
   next();
