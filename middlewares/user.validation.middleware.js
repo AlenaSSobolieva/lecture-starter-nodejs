f07b6532a435de1def8e147b3ac45b210d95d9aa
Olena Sobolieva <soboleva13as@gmail.com>
2026-05-31 23:57:31 +0300

    feat: implement user update validation

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -37,6 +37,26 @@ const createUserValid = (req, res, next) => {
 
 const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
+  const bodyFields = Object.keys(req.body);
+  if (bodyFields.includes("id")) {
+    throw Error("User entity to update isn't valid");
+  }
+   if (bodyFields.length === 0) {
+     throw Error("User entity to update isn't valid");
+  }
+  if (bodyFields.some((field) => !Object.keys(USER).includes(field))) {
+    throw Error("User entity to update isn't valid");
+  }
+  if (req.body.password && req.body.password.length < 3) {
+    throw Error("User entity to update isn't valid");
+  }
+  if (req.body.email && !req.body.email.endsWith("@gmail.com")) {
+    throw Error("User entity to update isn't valid");
+  }
+  if (req.body.phone && !/^\+380\d{9}$/.test(req.body.phone)) {
+    throw Error("User entity to update isn't valid");
+  }
+
   next();
 };
 
