74349d4b4d1bd29c72ad2731aa2797c17d61009e
Olena Sobolieva <soboleva13as@gmail.com>
2026-05-31 22:06:28 +0300

    feat: implement response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,23 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
-  next();
+  if (res.err) {
+    const message = res.err.message;
+
+    if (message.includes("not found")) {
+      return res.status(404).json({
+        error: true,
+        message,
+      });
+    }
+
+    return res.status(400).json({
+      error: true,
+      message,
+    });
+  }
+
+  return res.status(200).json(res.data);
 };
 
 export { responseMiddleware };
+
