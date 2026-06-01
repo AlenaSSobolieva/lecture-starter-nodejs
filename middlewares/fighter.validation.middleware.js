b190790a7c8f53ac9a72c45cc33cccedc3815169
Olena Sobolieva <soboleva13as@gmail.com>
2026-06-02 01:00:03 +0300

    Implement fighter routes and validation

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -2,11 +2,86 @@ import { FIGHTER } from "../models/fighter.js";
 
 const createFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during creation
+  const bodyFields = Object.keys(req.body);
+  const requiredFields = ["name", "power", "defense"];
+
+  if (bodyFields.includes("id")) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+  if (!requiredFields.every((field) => bodyFields.includes(field))) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+
+  if (bodyFields.some((field) => !Object.keys(FIGHTER).includes(field))) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+
+  if (req.body.power < 1 || req.body.power > 100) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+
+  if (req.body.defense < 1 || req.body.defense > 10) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+
+  if (
+    req.body.health !== undefined &&
+    (req.body.health < 80 || req.body.health > 120)
+  ) {
+    res.err = Error("Fighter entity to create isn't valid");
+    return next();
+  }
+
   next();
 };
 
 const updateFighterValid = (req, res, next) => {
   // TODO: Implement validatior for FIGHTER entity during update
+  const bodyFields = Object.keys(req.body);
+
+  if (bodyFields.includes("id")) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
+
+  if (bodyFields.length === 0) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
+
+  if (bodyFields.some((field) => !Object.keys(FIGHTER).includes(field))) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
+
+  if (
+    req.body.power !== undefined &&
+    (req.body.power < 1 || req.body.power > 100)
+  ) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
+
+  if (
+    req.body.defense !== undefined &&
+    (req.body.defense < 1 || req.body.defense > 10)
+  ) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
+
+  if (
+    req.body.health !== undefined &&
+    (req.body.health < 80 || req.body.health > 120)
+  ) {
+    res.err = Error("Fighter entity to update isn't valid");
+    return next();
+  }
   next();
 };
 
--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -9,5 +9,86 @@ import {
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const data = fighterService.getAll();
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = fighterService.getOne(req.params.id);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    if (res.err) {
+      return next();
+    }
+
+    try {
+      res.data = fighterService.create(req.body);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.patch(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    if (res.err) {
+      return next();
+    }
+
+    try {
+      res.data = fighterService.update(req.params.id, req.body);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      res.data = fighterService.delete(req.params.id);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware,
+);
 
 export { router };
